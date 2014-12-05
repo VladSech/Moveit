607163d3058acaae703164af8361d52182715311
VladSech <Sechkinvlad@gmail.com>
2014-12-05 14:02:27 +0200

    Trying to add header into header.php

---
+++ php/header.php
@@ -0,0 +1,65 @@
+<div id="header" class="header">
+    <div class="clearfix">
+        <a href="../index.html" class="logo-header"><img src="../images/logo.png" /></a>
+        <h1 class="head">Туризма сайт</h1>
+        <div class="search">
+            <label for="searching"></label>
+            <form class="searchform" action="">
+                <input id="searching" type="text" name="searching" value="Поиск по сайту" />
+                <input type="submit" value="Найти">
+            </form>
+        </div>
+        <div class="main-menu">
+            <ul>
+                <li>
+                    <a href="../index.html">
+                        <div class="icon"></div>
+                        Главная
+                    </a>
+                </li>
+                <li><a href="#action-menu">
+                        <div class="icon icon-action"></div>
+                        <br/>Мероприятия</a></li>
+                <li><a href="#photo-menu">
+                        <div class="icon icon-photo"></div>
+                        <br/>Фото</a></li>
+                <li><a href="#video-menu">
+                        <div class="icon icon-video"></div>
+                        <br/>Видео</a></li>
+                <li><a href="#article-menu">
+                        <div class="icon icon-article"></div>
+                        <br/>Статьи</a></li>
+                <li><a href="#">
+                        <div class="icon icon-market"></div>
+                        <br/>Барахолка</a></li>
+                <li><a href="#">
+                        <div class="icon icon-contact"></div>
+                        <br/>Контакты</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="action-menu" class="hide">
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+    <div id="photo-menu" class="hide">
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+    <div id="video-menu" class="hide">
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+    <div id="article-menu" class="hide">
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+</div>
